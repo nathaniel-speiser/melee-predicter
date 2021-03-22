1eedfcf3df2ceb9a358e6ec95c74309d67a27771
Nathaniel <nathaniel.speiser@gmail.com>
2021-03-21 20:31:21 -0600

    Streamlit test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ from utils.GameStats import GameStats
 st.set_page_config(layout='wide')
 
 import os
+
+for root, dirs, files in os.walk("."):
+    for filename in files:
+        print(filename)
 dir = os.path.dirname(__file__)
 f1 = os.path.join(dir, 'saved_data/streamlit_model.pkl')
 f2 = os.path.join(dir, 'saved_data/features.pkl')
